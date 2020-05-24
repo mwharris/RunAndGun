898f2ebb8f08c7452d137d644fda4334f2d157e5
mharris161 <mharris161@gmail.com>
2020-05-24 15:39:34 -0400

    Forgot to commit the new camera controller.

---
+++ Assets/Rewrite Scripts/Player/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    private Transform _cameraTransform;
+    
+    private float _cameraDefaultFov;
+    private const float CameraSprintFov = 65f;
+
+    public bool PlayerIsSprinting { get; set; } = false;
+    
+    private bool _playerIsGrounded = false;
+    private bool _playerIsWallRunning = false;
+    private bool _playerJumped = false;
+    private bool _playerIsAiming = false;
+
+    private void Awake()
+    {
+        _cameraTransform = _camera.transform;
+        _cameraDefaultFov = _camera.fieldOfView;
+    }
+
+    private void Update()
+    {
+        HandleSprinting();
+    }
+
+    private void HandleSprinting()
+    {
+        if (PlayerIsSprinting || _playerIsWallRunning)
+        {
+            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, CameraSprintFov, Time.deltaTime * 5f);
+        }
+        else if (_camera.fieldOfView != _cameraDefaultFov)
+        {
+            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, _cameraDefaultFov, Time.deltaTime * 5f); ;
+        }
+    }
+    
+}
