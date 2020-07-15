7bea5b92ae53d6e8b1f394763f50a6da1efadfd2
mharris161 <mharris161@gmail.com>
2020-07-15 08:02:56 -0400

    Added a script to test framerate dependency. Fixed an issue with not being able to wall-run along the red container anymore

---
+++ Assets/Rewrite Scripts/TargetFramerate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFramerate : MonoBehaviour
+{
+    public int target = 60;
+    
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+    }
+
+    private void Update()
+    {
+        if (target != Application.targetFrameRate)
+        {
+            Application.targetFrameRate = target;
+        }
+    }
+}
